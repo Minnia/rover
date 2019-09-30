61162aafed7bb8e0dbc29ae89109a6adf8e1da53
Jasmine Pishnemazi <Jasmine@Tylers-MacBook-Pro-2.local>
2019-09-30 16:27:01 +0200

    added function to read input

--- rover.py
+++ rover.py
@@ -12,6 +12,8 @@ class Rover(object):
     commands = {"L": "Left", "R": "Right", "M": "Move forward"}
     directions = {"N": 1, "S": 3, "E": 2, "W": 4}
     heading = directions["N"]
+    x = 0
+    y = 0
 # Initialize plateau, or grid, for Rover
 
     def __init__(self, x, y, position, heading, commands):
@@ -25,25 +27,27 @@ class Rover(object):
         return f"Coordinates of Rover is {self.x},{self.y}. {self.position}. Heading of Rover is {self.heading}. Commands of rover are {self.commands}"
         # Initial position of Rover
 
-    def set_position(self, x, y, position, heading):
-        self.x = x
-        self.y = y
-        self.position = Position(x, y)
-        self.heading = heading
+    @classmethod
+    def set_position(cls, position, heading, coordinate_x=0, coordinate_y=0):
+        cls.position = Position()
+        cls.heading = heading
+        cls.coordinate_x = coordinate_x
+        cls.coordinate_y = coordinate_y
 
     def moveForward(self, x, y, direction):
         self.x = Position(x)
         self.y = Position(y)
-        if direction == "N":
-            y += 1
-        elif direction == "S":
-            y -= 1
-        elif direction == "W":
-            x -= 1
-        elif direction == "E":
-            x += 1
-        else:
-            raise ValueError("That's not possible!")
+        for dirs in direction:
+            if dirs == "N":
+                y += 1
+            elif dirs == "S":
+                y -= 1
+            elif dirs == "W":
+                x -= 1
+            elif dirs == "E":
+                x += 1
+            else:
+                raise ValueError("That's not a valid direction")
 
     def rotate_rover(self, command):
         self.command = command
@@ -68,5 +72,26 @@ class Rover(object):
             return self.heading+1
 
 
-r = Rover(5, 5, (Position()), "N", "LMLMLMLMM")
+def readfile():
+    try:
+        f = open("input.txt", "r")
+        li = []
+        if f.mode == "r":
+            for line in f:
+                li.append(line.replace("\n", ""))
+        f.close()
+        if len(li) > 0:
+            return li
+    except:
+        raise TypeError("Nothing to show")
+
+
+r = Rover(0, 0, (Position()), "N", "LMLMLMLMM")
+
 print(r)
+r.moveForward(1, 2, "S")
+print(r)
+r.moveForward(1, 2, "N")
+print(r)
+readfile()
+# r.rotate_rover("L")
